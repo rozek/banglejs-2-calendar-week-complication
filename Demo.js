74b156131953dbbea9da9e0623213b7bd7e5d392
Andreas Rozek <A.Rozek@gmx.de>
2022-01-12 06:05:24 +0100

    Update Demo.js

--- Demo.js
+++ Demo.js
@@ -7,5 +7,6 @@
       t:require('https://raw.githubusercontent.com/rozek/banglejs-2-calendar-week-complication/main/Complication.js'),
     }
   },{
-    Foreground:'#000000', Background:'#FFFFFF', Seconds:'#FF0000'
+    Foreground:'#000000', Background:'#FFFFFF', Seconds:'#FF0000',
+    withDots:true
   });
